6ae0cd471f5c1b775e157943c1e179aa19e72118
Jay Kim <kimjaehu@gmail.com>
2019-02-12 21:15:24 +0000

    r /bit /bin/bash: q: command not found

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-let express = require("express");
+const express = require("express");
 let app = express();
 const PORT = 8080;
 const bodyParser = require( "body-parser" );
@@ -28,20 +28,33 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-});
-
 app.post( "/" , ( req , res ) => {
 	res.render( "urls_new" , { users : usersdb.findByName( req.body.filter ) });
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+    res.render("urls_show", templateVars);
+});
 
 app.post("/urls", (req, res) => {
     console.log(req.body);  // Log the POST request body to the console
     res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });  
 
+app.get("/u/:shortURL", (req, res) => {
+    const longURL = req.body.longURL
+    res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+    Math.random().toString(36).substring(7)
+}
